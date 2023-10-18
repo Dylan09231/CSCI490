24dadd2698839c7d9b7920948a93a26b5ceaffd3
Dylan Tannery <147246099+Dylan09231@users.noreply.github.com>
2023-10-18 12:27:15 -0400

    Add files via upload
    starting object creation

---
+++ objects.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+//classes
+public class objects
+{
+	//objects
+	public wallets()
+	{
+		//stores currency user invested in
+		private List<string> curencies;
+
+		//stores U.S dollar equivalent
+		private List<double> dollarList;
+		
+		private string userId;
+
+		//overall value of wallet in dollars 
+		private double overallValueDollar;
+		
+		//constructor
+		wallets(string user)
+		{
+			this.curencies = new List<string>;
+			this.dollarList = new List<double>;
+			this.userId = user;
+			this.overallValueDollar = 0.00;  
+		}
+
+		addCryptoToWallet(string crypto, double dollar, double crypto)
+		{ 	
+			this.curencies.add(crypto);
+			this.dollarList.add(cost);
+			this.overallValueDollar += cost; 
+		}
+
+		
+		setCurencies(List<string> updated values)
+		{
+             //update currencies		 
+
+		}
+
+
+
+			
+}
+
+
+
